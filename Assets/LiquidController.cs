40b79440b60ecaece9217ca7cc9f17a2061e5d97
tatsupiko <identity2022@outlook.jp>
2022-10-05 16:31:29 +0900

    流体シミュレーションに必要なパラメータの追加、密度の計算式を追加

--- Assets/LiquidController.cs
+++ Assets/LiquidController.cs
@@ -2,11 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//参考資料
+//https://soysoftware.sakura.ne.jp/archives/1559
+
+class ParticleElements
+{
+    /// <summary>座標</summary>
+    public Vector3 position;
+    /// <summary>速度</summary>
+    public Vector3 velocity;
+    /// <summary>力</summary>
+    public Vector3 force;
+    /// <summary>密度</summary>
+    public float density;
+    /// <summary>圧力</summary>
+    public float pressure;
+}
+
 public class LiquidController : MonoBehaviour
 {
+    /// <summary>影響範囲</summary>
+    [SerializeField] float _areaOfInfluence = 0f;
+    /// <summary>粒子の質量</summary>
+    [SerializeField] float _particleMass = 1f;
+    /// <summary>密度</summary>
+    [SerializeField] float _density = 1f;
     void Start()
     {
-        
+        _density = 315 / 64 * Mathf.PI * Mathf.Pow(_areaOfInfluence, 9);  //密度の計算
     }
 
     void Update()
