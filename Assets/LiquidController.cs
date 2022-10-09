6112b9f2dd056430eae6573946ba8adc9d16d24b
tatsupiko <identity2022@outlook.jp>
2022-10-09 22:31:07 +0900

    圧力の計算式を追加

--- Assets/LiquidController.cs
+++ Assets/LiquidController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 //参考資料
 //https://soysoftware.sakura.ne.jp/archives/1559
 
+/// <summary>粒子</summary>
 class ParticleElements
 {
     /// <summary>座標</summary>
@@ -25,7 +26,7 @@ class LiquidController : MonoBehaviour
     [SerializeField] float _areaOfInfluence = 0f;
     /// <summary>粒子の質量</summary>
     [SerializeField] float _particleMass = 1f;
-    /// <summary>密度の計算</summary>
+    /// <summary>密度の計算で使う</summary>
     [SerializeField] float _density = 1f;
 
     void Start()
@@ -33,22 +34,24 @@ class LiquidController : MonoBehaviour
         _density = 315 / 64 * Mathf.PI * Mathf.Pow(_areaOfInfluence, 9);  //密度の計算
     }
 
+    //ー－－－－－ー密度に関する処理系ー－－ー－－－
+
     /// <summary>粒子の密度の計算</summary>
-    /// <param name="particle">粒子の要素</param>
-    void CalcDencity(ParticleElements[] particle)
+    /// <param name="particles">粒子の要素</param>
+    void CalcDencity(ParticleElements[] particles)
     {
         float aoi2 = _areaOfInfluence * _areaOfInfluence;  //_areaOfInfluence の2乗を計算しておく
 
-        for(int i = 0; i < particle.Length; i++)
+        for(int i = 0; i < particles.Length; i++)
         {
-            var nowParticle = particle[i];
+            var nowParticle = particles[i];
             float sum = 0f;
 
-            for(int j = 0; j < particle.Length; j++)
+            for(int j = 0; j < particles.Length; j++)
             {
-                if (i == j) continue;
+                if (i == j) continue;  //判定しているのが自分自身だったら、スキップする
 
-                var nearParticle = particle[j];
+                var nearParticle = particles[j];
                 Vector3 particleDistence = nearParticle.position - nowParticle.position;
                 float pd2 = Vector3.Dot(particleDistence, particleDistence);
 
@@ -61,18 +64,35 @@ class LiquidController : MonoBehaviour
         }
     }
 
+    //ー－－－ー－－圧力に関する処理系ー－－－ー－－
+
     /// <summary>圧力係数</summary>
     [SerializeField] float _pressureCoefficient = 200f;
     /// <summary>外力がかかってないときの密度</summary>
     [SerializeField] float _restDensity = 1000f;
+    [SerializeField] float _pressure = 1f;
 
     /// <summary>粒子にかかる圧力を計算</summary>
     /// <param name="particles">粒子の要素</param>
     void CalcPressure(ParticleElements[] particles)
     {
+        float aoi2 = _areaOfInfluence * _areaOfInfluence;
+
         for(int i = 0; i < particles.Length; i++)
         {
             particles[i].pressure = _pressureCoefficient * (particles[i].density - _restDensity);
+        }
+        _pressure = 45 / Mathf.PI * Mathf.Pow(_areaOfInfluence, 6);
+
+        for(int i = 0; i < particles.Length; i++)
+        {
+            var nowParticle = particles[i];
+            float sum = 0f;
+
+            for(int j = 0; j < particles.Length; j++)
+            {
+                if (i == j) continue;
+            }
         }
     }
 }
