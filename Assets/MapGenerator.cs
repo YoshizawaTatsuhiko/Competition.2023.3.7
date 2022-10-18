fd08d3016b641f49e951bd1082df5551bc25ebfe
tatsupiko <identity2022@outlook.jp>
2022-10-18 23:39:09 +0900

    Map Generator の作成 (未完成)

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    List<(int, int)> _startGenerateWalls = new List<(int, int)>();
+    List<(int, int)> _goalPoint = new List<(int, int)>();
     public void CreateMap(int width, int height)
     {
         int w = width % 2 == 0 ? width : width - 1;
         int h = height % 2 == 0 ? height : height - 1;
+
+        for (int i = 0; i < w; i++)
+            for (int j = 0; j < h; j++)
+            {
+
+            }
     }
 }
