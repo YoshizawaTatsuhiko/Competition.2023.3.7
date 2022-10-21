406c406eb8411c47f84d6be86068df29af039ec8
tatsupiko <identity2022@outlook.jp>
2022-10-21 23:56:21 +0900

    ゴール地点候補を格納するリストの追加

--- Assets/RandomMapController.cs
+++ Assets/RandomMapController.cs
@@ -7,6 +7,7 @@ public class RandomMapController : MonoBehaviour
 {
     /// <summary>壁生成開始地点候補</summary>
     List<(int, int)> _startPoint = new List<(int, int)>();
+    List<(int, int)> _goalPosition = new List<(int, int)>();
 
     /// <summary>マップを自動生成する</summary>
     /// <param name="width">横幅</param>
