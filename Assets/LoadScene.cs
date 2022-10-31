e5f2dc46946ae1ff51015cdab950528983891375
tatsupiko <identity2022@outlook.jp>
2022-10-31 19:25:26 +0900

    LoadSceneにコメントを追加

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -7,18 +7,27 @@ using DG.Tweening;
 
 public class LoadScene : MonoBehaviour
 {
-    [SerializeField] float _fadeTime = 1f;
-    [SerializeField] Image _image = default;
+    [Header("Fade in/out するまでの時間")]
+    [Tooltip("Fade in/out するまでの時間")] [SerializeField] float _fadeTime = 1f;
+    [Header("Fade in/out させるPanel")]
+    [Tooltip("Fade in/out させるPanel")]    [SerializeField] Image _image = default;
 
     void Start()
     {
-        _image.color = Color.black;
+        //Panelを見えるようにしておく
+        _image?.CrossFadeAlpha(1, 0, false);
+        //Fade in 完了後、Panelを消す
         _image.enabled = true;
         _image.DOFade(0f, _fadeTime).OnComplete(() => _image.enabled = false);
     }
 
+    /// <summary>シーンを遷移させる</summary>
+    /// <param name="sceneName">シーンの名前</param>
     public void SceneToLoad(string sceneName)
     {
+        //Panelを見えなくしておく
+        _image.CrossFadeAlpha(1, 0, false);
+        //Fade out 完了後、シーンを遷移する
         _image.enabled = true;
         _image.DOFade(1f, _fadeTime).OnComplete(() => SceneManager.LoadScene(sceneName));
     }
