f2044b0e42ee5c9c4285cf7ca79e764bf1d76566
tatsupiko <identity2022@outlook.jp>
2022-12-01 21:02:12 +0900

    SightControllerの更新

--- Assets/Program/SightController.cs
+++ Assets/Program/SightController.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider))]
-
 public class SightController : MonoBehaviour
 {
-    /// <summary>索敵用のSphereCollider</summary>
-    private SphereCollider _sphere = default;
+    [SerializeField, Tooltip("索敵する対象")]
+    private Transform _terget;
+    [SerializeField, Tooltip("索敵範囲")]
+    private float _searchAngle = 1f;
+    [SerializeField, Tooltip("索敵距離")]
+    private float _range = 1f;
+    /// <summary>衝突したオブジェクトの情報</summary>
+    private RaycastHit _hit;
 
-    void Start()
+    private void FixedUpdate()
     {
-        _sphere = GetComponent<SphereCollider>();
-        _sphere.isTrigger = true;
-    }
+        float distance = Vector3.Distance(_terget.position, transform.position);
+        Vector3 vec = _terget.position - transform.position;
+        float angle = Vector3.Angle(vec, transform.forward);
 
-    void Update()
-    {
-        
+        if (angle <= _searchAngle && distance <= _range)
+        {
+            transform.forward = _terget.position;
+            Physics.Raycast(transform.position, vec, out _hit, _range, LayerMask.GetMask(""));
+        }
     }
 }
