0e94610a7ee662c01fcd473bf9764f18d5771415
tatsupiko <identity2022@outlook.jp>
2023-01-10 23:56:29 +0900

    SightControllerを修正中。

--- Assets/Program/SightController.cs
+++ Assets/Program/SightController.cs
@@ -6,116 +6,29 @@ using MyGizmos;  //自作名前空間：Gizmosを追加する
 
 public class SightController : MonoBehaviour
 {
-    [SerializeField, Tooltip("索敵する対象")]
-    private Transform _terget;
-    [SerializeField, Range(0f, 1f), Tooltip("索敵範囲")]
-    private float _searchAngle = 1f;
-    [SerializeField, Tooltip("索敵距離")]
-    private float _range = 1f;
-    /// <summary>対象との距離</summary>
-    private float _distance;
-    /// <summary>対象を発見したかどうかを判定する</summary>
-    private bool _isDiscover = false;
-    /// <summary>対象見失ったかどうかを判定する</summary>
+    [SerializeField]
+    private Transform _target = null;
+    [SerializeField, Range(0f, 1f)]
+    private float _searchDeg = 1f;
+    [SerializeField]
+    private float _searchDistance = 1f;
+    // ターゲットとの距離
+    private float _distanceToTarget = 0f;
+    // ターゲットを発見したかどうかを判定する
+    private bool _isFind = false;
+    // ターゲットを見失ったかどうかを判定する
     private bool _isTergetLost = false;
-    /// <summary>対象を見失った時に向き直る方向</summary>
-    private Vector3 _front = Vector3.zero;
-    /// <summary>振り向くまでの時間</summary>
-    private float _turnTime = 0.5f;
+    // ターゲットを見失った時に向き直る方向
+    private Vector3 _turnAroundDir = Vector3.zero;
 
     private void Start()
     {
-        if (!_terget) Debug.LogWarning("対象がassignされていません。");
+        if (!_target) Debug.LogWarning("対象がassignされていません。");
     }
 
     private void FixedUpdate()
     {
-        //_distance = Vector3.Distance(_terget.transform.position, transform.position);
-
-        //対象を発見した時
-        if(_isDiscover)
-        {
-            _front = transform.forward;
-
-            //Rayを飛ばして、対象との間に障害物があるかどうか調べる
-            if (Physics.Raycast(transform.position, transform.forward * _range, _terget.gameObject.layer))
-            {
-                Debug.DrawRay(transform.position, transform.forward * _range);
-                transform.LookAt(Vector3.Lerp(transform.forward + transform.position, _terget.position, _turnTime));
-                Debug.Log("Look...");
-            }
-            else
-            {
-                _isDiscover = false;
-                _isTergetLost = true;
-                Debug.Log("Switch");
-            }
-        }
-
-        //対象を発見できていない時
-        else
-        {
-            //対象を発見するときに使う内積
-            float discoverDot = Mathf.Abs(Vector3.Dot(
-                transform.forward, (_terget.position - transform.position).normalized));
-
-            if (discoverDot >= _searchAngle)
-            {
-                _isDiscover = true;
-                Debug.Log("Discover!");
-            }
-            if (_isTergetLost)
-            {
-                //対象を見失った時、発見する以前の方向を向くときに使う内積
-                float lookDot = Vector3.Dot(transform.forward, _front);
-
-                if (lookDot <= 0.95f)
-                {
-                    transform.LookAt(
-                        Vector3.Lerp(transform.forward + transform.position, _front + transform.position, _turnTime));
-                }
-                else  //ある程度発見前の方向を向いたら、振り向くのをやめる
-                {
-                    _isTergetLost = false;
-                }
-            }
-            //Debug.Log("(暇だなぁ...)");
-        }
+        Vector3 toTarget = transform.position - _target.position;
+        float dot = Vector3.Dot(transform.forward, toTarget);
     }
-
-    /*
-    /// <summary>Tergetが視界に入っているかどうか判定する</summary>
-    /// <param name="angle">索敵範囲</param>
-    /// <returns>入っている -> true | 入っていない -> false;</returns>
-    private bool SearchTerget(float angle)
-    {
-        //Tergetとゲームオブジェクトの内積を計算する
-        float dot = Vector3.Dot(transform.forward, (_terget.position - transform.position).normalized);
-
-        //Playerが視界の範囲内に入った時の処理
-        if(Mathf.Abs(dot) >= angle)
-        {
-            return true;
-        }
-
-        //Playerが視界の範囲外に居る時の処理
-        else
-        {
-            return false;
-        }
-    }
-
-    /// <summary>ターゲットとゲームオブジェクトの間に障害物があるかどうかを判定する</summary>
-    /// <param name="terget">凝視する対象</param>
-    /// <returns>障害物ナシ -> true | 障害物アリ -> false</returns>
-    private void LookAtTerget(Transform terget)
-    {
-        //ターゲットの方向を向く
-        transform.LookAt(Vector3.Lerp(transform.forward + transform.position, terget.position, 1f));
-
-        //対象とゲームオブジェクトの間に障害物があるかどうかを確認するためのRayを飛ばす
-        Physics.Raycast(transform.position, transform.forward, terget.gameObject.layer);
-        Debug.DrawRay(transform.position, transform.forward);
-    }
-    */
 }
