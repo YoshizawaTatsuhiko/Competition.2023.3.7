08893731955c597dde083fd2d5f166aefd556226
tatsupiko <identity2022@outlook.jp>
2023-01-22 23:45:17 +0900

    迷路生成アルゴリズムで作成した二次元配列をもとにオブジェクトを配置するプログラムを作成。

--- Assets/Program/MazeGenerator.cs
+++ Assets/Program/MazeGenerator.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 
 class MazeGenerator : MonoBehaviour
 {
-    [SerializeField]
-    private int _width = 1;
-    [SerializeField]
-    private int _height = 1;
+    [SerializeField, Tooltip("MAX SIZE = 32")]
+    private int _size = 5;
+    private int _width = 0;
+    private int _height = 0;
     [SerializeField, Tooltip("–À˜H‚ğŒ`¬‚·‚éobject")]
     private GameObject[] _go = null;
     /// <summary>¶¬‚µ‚½–À˜H</summary>
     private MazeCreaterExtend _maze = null;
+    private string[,] _bluePrint;
 
     [SerializeField]
     private float _clickInterval = 2f;
@@ -21,7 +22,21 @@ class MazeGenerator : MonoBehaviour
 
     private void Start()
     {
+        _width = _size;
+        _height = _size;
         _maze = GetComponent<MazeCreaterExtend>();
+        string[] mazeInfo = _maze.GenerateMaze(_width, _height).Split("\n");
+        _bluePrint = new string[mazeInfo[0].Length, mazeInfo.Length - 1];
+        To2DArray(mazeInfo, _bluePrint);
+
+        for (int i = 0; i < _bluePrint.GetLength(0); i++)
+        {
+            for (int j = 0; j < _bluePrint.GetLength(1); j++)
+            {
+                if (_bluePrint[i, j] == "W") Instantiate(_go[0], new Vector3(i, 0, j), Quaternion.identity);
+                if (_bluePrint[i, j] == "F") Instantiate(_go[1], new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
     }
 
     private bool _frag = true;
@@ -30,16 +45,18 @@ class MazeGenerator : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1) && _frag)
         {
-            string[] mazeInfo = _maze.GenerateMaze(_width, _height).Split("\n");
+            //string[] mazeInfo = _maze.GenerateMaze(_width, _height).Split("\n");
+            //_bluePrint = new string[mazeInfo[0].Length, mazeInfo.Length];
+            //To2DArray(mazeInfo, _bluePrint);
 
-            for (int i = 0; i < _height; i++)
-            {
-                for(int j = 0; j < _width; j++)
-                {
-                    if (mazeInfo[i][j] == 'W') Instantiate(_go[0]);
-                    if (mazeInfo[i][j] == 'F') Instantiate(_go[1]);
-                }
-            }
+            //for (int i = 0; i < _height; i++)
+            //{
+            //    for(int j = 0; j < _width; j++)
+            //    {
+            //        if (_bluePrint[i, j] == "W") Instantiate(_go[0], new Vector3(i, 0, j), Quaternion.identity);
+            //        if (_bluePrint[i, j] == "F") Instantiate(_go[1], new Vector3(i, 0, j), Quaternion.identity);
+            //    }
+            //}
             _frag = false;
         }
         else
@@ -52,5 +69,20 @@ class MazeGenerator : MonoBehaviour
                 _timer = 0f;
             }
         }
+    }
+
+    /// <summary>ˆêŸŒ³”z—ñ‚ğ“ñŸŒ³”z—ñ‚É•ÏŠ·‚·‚é(stringŒ^ŒÀ’è)</summary>
+    /// <param name="array">stringŒ^‚Ì”z—ñ</param>
+    /// <param name="twoDimensionalArray">stringŒ^‚Ì“ñŸŒ³”z—ñ</param>
+    private string[,] To2DArray(string[] array, string[,] twoDimensionalArray)
+    {
+        for (int i = 0; i < twoDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < twoDimensionalArray.GetLength(1); j++)
+            {
+                twoDimensionalArray[i, j] = array[i][j].ToString();
+            }
+        }
+        return twoDimensionalArray;
     }
 }
