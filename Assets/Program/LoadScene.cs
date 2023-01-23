c30fff0eea79be15b7fdade08150e0fda978d370
tatsupiko <identity2022@outlook.jp>
2023-01-23 16:26:36 +0900

    サンプルプログラムの整理

--- Assets/Program/LoadScene.cs
+++ Assets/Program/LoadScene.cs
@@ -10,25 +10,31 @@ public class LoadScene : MonoBehaviour
     [Header("Fade in/out するまでの時間")]
     [Tooltip("Fade in/out するまでの時間")] [SerializeField] float _fadeTime = 1f;
     [Header("Fade in/out させるPanel")]
-    [Tooltip("Fade in/out させるPanel")]    [SerializeField] Image _image = default;
+    [Tooltip("Fade in/out させるPanel")]    [SerializeField] Image _image = null;
 
     void Start()
     {
-        //Panelを見えるようにしておく
-        _image?.CrossFadeAlpha(1, 0, false);
-        //Fade in 完了後、Panelを消す
-        _image.enabled = true;
-        _image.DOFade(0f, _fadeTime).OnComplete(() => _image.enabled = false);
+        if (_image != null)
+        {
+            //Panelを見えるようにしておく
+            _image.CrossFadeAlpha(1, 0, false);
+            //Fade in 完了後、Panelを消す
+            _image.enabled = true;
+            _image.DOFade(0f, _fadeTime).OnComplete(() => _image.enabled = false);
+        }
     }
 
     /// <summary>シーンを遷移させる</summary>
     /// <param name="sceneName">シーンの名前</param>
     public void SceneToLoad(string sceneName)
     {
-        //Panelを見えなくしておく
-        _image?.CrossFadeAlpha(1, 0, false);
-        //Fade out 完了後、シーンを遷移する
-        _image.enabled = true;
-        _image.DOFade(1f, _fadeTime).OnComplete(() => SceneManager.LoadScene(sceneName));
+        if (_image != null)
+        {
+            //Panelを見えなくしておく
+            _image.CrossFadeAlpha(1, 0, false);
+            //Fade out 完了後、シーンを遷移する
+            _image.enabled = true;
+            _image.DOFade(1f, _fadeTime).OnComplete(() => SceneManager.LoadScene(sceneName));
+        }
     }
 }
