5c9beb2d87e39c4fd0cd1f36a4ba9db4e2fa0450
tatsupiko <identity2022@outlook.jp>
2023-01-28 01:59:54 +0900

    「PlayerController」の挙動をCharacter Controller から Rigidbody に変更した。

--- Assets/Program/PlayerController.cs
+++ Assets/Program/PlayerController.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField,Tooltip("ˆÚ“®‘¬“x")] float _speed = 1.0f;
-    CharacterController _character = null;
+    [SerializeField,Tooltip("ˆÚ“®‘¬“x")]
+    private float _speed = 1.0f;
+    private Rigidbody _rigidbody = null;
 
     void Start()
     {
-        _character = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         dir = Camera.main.transform.TransformDirection(dir);
         dir.y = 0;
-        _character.SimpleMove(dir.normalized * _speed);
+        _rigidbody.velocity = dir * _speed;
         Vector3 camDir = Camera.main.transform.forward;
         camDir.y = 0;
         transform.forward = camDir;
