a9e30102708aaae929a3c587c06afeed9d84f307
tatsupiko <identity2022@outlook.jp>
2023-02-06 20:24:46 +0900

    ターゲットが見つからなかったときはウロウロし、見つかった時はターゲットに近づく処理を追加した。

--- Assets/Program/EnemyController.cs
+++ Assets/Program/EnemyController.cs
@@ -2,23 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(SightController))]
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 1f;
-    /// <summary>Rigidbody</summary>
     private Rigidbody _rb = null;
+    private SightController _sight = null;
+    [SerializeField]
+    private float _waitTime = 3f;
+    /// <summary>ウロウロするときに向く方向</summary>
+    private Vector3 _direction = Vector3.zero;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        _sight = GetComponent<SightController>();
     }
 
-    private void Update()
+    private float _timer = 0f;
+    private bool _frag = true;
+
+    private void FixedUpdate()
     {
+        if (_sight.SearchTarget())  // ターゲットを発見したとき
+        {
+            if (!_sight.LookTarget())  // ターゲットとの間に障害物がある時
+            {
+                _timer += Time.fixedDeltaTime;
+                if (_timer > _waitTime)  // ターゲットが障害物に一定時間隠れたら、索敵しなおす。
+                {
+                    _timer = 0f;
+                    return;
+                }
+            }
+        }
+        else  // ターゲットが発見できていないとき
+        {
+            if(_frag) StartCoroutine(Wander());
+        }
         _rb.velocity = transform.forward * _moveSpeed;
+    }
+
+    /// <summary>オブジェクトがウロウロする</summary>
+    private IEnumerator Wander()
+    {
+        _frag = false;
+        Debug.Log("Call");
+        Vector3 random = Random.insideUnitSphere;
+        _direction = new Vector3(random.x, 0f, random.z).normalized;
+        transform.forward = _direction;
+
+        yield return new WaitForSeconds(_waitTime);
+        _frag = true;
     }
 }
