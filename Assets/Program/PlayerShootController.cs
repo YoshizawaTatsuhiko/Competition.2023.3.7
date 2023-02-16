3a0b0ad306829be2e6236b3ddd69f29737f6ac5b
tatsupiko <identity2022@outlook.jp>
2023-02-16 23:54:37 +0900

    MainCameraの参照をScene再生中以外でも獲得できるようにした。

--- Assets/Program/PlayerShootController.cs
+++ Assets/Program/PlayerShootController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 // 日本語対応
-public class PlayerShootController : ShootBase
+[ExecuteInEditMode, RequireComponent(typeof(LineRenderer))]
+
+public class PlayerShootController : MonoBehaviour
 {
+    private Camera _mainCamera = null;
     [SerializeField]
     private Transform _muzzle = null;
     [SerializeField]
@@ -18,10 +22,17 @@ public class PlayerShootController : ShootBase
     private float _shootRange = 1f;
     [SerializeField]
     private LayerMask _layerMask = 0;
+    private LineRenderer _line = null;
+
+    private void Start()
+    {
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _line = GetComponent<LineRenderer>();
+    }
 
     private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(_crosshair.rectTransform.position);
+        Ray ray = _mainCamera.ScreenPointToRay(_mainCamera.transform.position);
         Vector3 hitPosition = _muzzle.transform.position + _muzzle.transform.forward * _shootRange;
 
         if (Physics.Raycast(ray, out RaycastHit hit, _shootRange, _layerMask))
@@ -34,13 +45,34 @@ public class PlayerShootController : ShootBase
             _crosshair.color = _defaultColor;
         }
 
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton/*Down*/(0))
         {
-            DrawRay(_muzzle.position, hitPosition);
+            //DrawRay(_muzzle.position, hitPosition);
+            DrawLaser(hitPosition);
+            //StartCoroutine(WaitForSeconds());
         }
         else
         {
-            DrawRay(_muzzle.position, _muzzle.position);
+            //DrawRay(_muzzle.position, _muzzle.position);
+            DrawLaser(_muzzle.position);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(_mainCamera.transform.position, _mainCamera.transform.forward * _shootRange);
+    }
+
+    private void DrawLaser(Vector3 destination)
+    {
+        Vector3[] positions = { _muzzle.position, destination };
+        _line.positionCount = positions.Length;
+        _line.SetPositions(positions);
+    }
+
+    private IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(0.1f);
     }
 }
