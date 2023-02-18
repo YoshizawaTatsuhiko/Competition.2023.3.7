66953fa4827012e79ed0401c466b6de833a677e7
tatsupiko <identity2022@outlook.jp>
2023-02-18 23:46:53 +0900

    GameManagerに「制限時間が無くなったら、ゲームオーバーになる」処理を追加した。

--- Assets/Program/GameManager.cs
+++ Assets/Program/GameManager.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    private float _timeLimit = 60f;
+    [SerializeField]
+    private Text _timeText = null;
+    [SerializeField]
     private UnityEvent _gameClear = new();
     [SerializeField]
     private UnityEvent _gameOver = new();
-    private GameObject _player = null;
     /// <summary>ゲームが始まった時にPlayerを生成する座標</summary>
     private Vector3 _startPoint = new();
     /// <summary>Goalを出現するために起動するGimmick</summary>
-    private GimmickCheck _gimmick = null;
+    private GimmickCheck _gimmickCheck = null;
     private GoalController _goal = null;
 
     private void Start()
@@ -26,26 +30,28 @@ public class GameManager : MonoBehaviour
         _startPoint.y += 1f;
 
         // ResourcesフォルダーからPlayerを生成する。
-        _player = Instantiate(Resources.Load<GameObject>("Player Prefab"), _startPoint, Quaternion.identity);
+        Instantiate(Resources.Load<GameObject>("Player Prefab"), _startPoint, Quaternion.identity);
         Instantiate(Resources.Load<GameObject>("Marker"));
 
-        _gimmick = FindObjectOfType<GimmickCheck>();
+        _gimmickCheck = FindObjectOfType<GimmickCheck>();
         _goal = FindObjectOfType<GoalController>();
         _goal.gameObject.SetActive(false);
     }
 
     private void FixedUpdate()
     {
+        _timeLimit = Mathf.Clamp(_timeLimit -= Time.fixedDeltaTime, 0f, _timeLimit);
+        _timeText.text = _timeLimit.ToString("F2");
+
         // 起動したGimmickの数が一定数に達したら、Goalを出現させる。
-        if (_gimmick.GimmickWakeUpCount == 2)
+        if (_gimmickCheck.GimmickWakeUpCount == 2)
         {
             _goal.gameObject.SetActive(true);
         }
-
-        if (_goal.GoalJudge)
-        {
-            GameClear();
-        }
+        // Playerがゴールに到着したら、ゲームクリア
+        if (_goal.GoalJudge) GameClear();
+        // 制限時間がなくなったら、ゲームオーバー
+        if (_timeLimit == 0f) GameOver();
     }
 
     /// <summary>ゲームクリアした時にEventを呼ぶ</summary>
@@ -59,6 +65,8 @@ public class GameManager : MonoBehaviour
     /// <summary>ゲームオーバーした時にEventを呼ぶ</summary>
     private void GameOver()
     {
+        Cursor.visible = true;
         _gameOver.Invoke();
+        Debug.Log("Mission Failed");
     }
 }
