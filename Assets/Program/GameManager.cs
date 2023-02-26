0e707a253c97d5708ce92ddc7d1b2b1a5d8fda68
tatsupiko <identity2022@outlook.jp>
2023-02-26 22:14:15 +0900

    Gamemanagerに「ゲーム開始時にマウスカーソルの非表示と固定化する」処理と「ゲームクリア or ゲームオーバー時にマウスカーソルを表示と固定化解除する」処理を追加した。

--- Assets/Program/GameManager.cs
+++ Assets/Program/GameManager.cs
@@ -16,42 +16,52 @@ public class GameManager : MonoBehaviour
     private UnityEvent _gameOver = new();
     /// <summary>ゲームが始まった時にPlayerを生成する座標</summary>
     private Vector3 _startPoint = new();
+    private HPController _playerHP = new HPController();
     /// <summary>Goalを出現するために起動するGimmick</summary>
     private GimmickCheck _gimmickCheck = null;
     private GoalController _goal = null;
 
+    private void OnEnable()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void Start()
     {
-        // ゲーム開始時にマウスカーソルを見えなくする。
-        Cursor.visible = false;
-
-        // ゲーム開始地点の座標を見つけておく
+        // Playerを生成する座標を見つけておく。
         _startPoint = GameObject.FindGameObjectWithTag("StartPoint").transform.position;
         _startPoint.y += 1f;
 
         // ResourcesフォルダーからPlayerを生成する。
-        Instantiate(Resources.Load<GameObject>("Player Prefab"), _startPoint, Quaternion.identity);
+        GameObject player = Instantiate(Resources.Load<GameObject>("Player Prefab"), _startPoint, Quaternion.identity);
+        if (player.TryGetComponent(out HPController hp)) _playerHP = hp;
         Instantiate(Resources.Load<GameObject>("Marker"));
 
         _gimmickCheck = FindObjectOfType<GimmickCheck>();
         _goal = FindObjectOfType<GoalController>();
-        _goal.gameObject.SetActive(false);
+        _goal?.gameObject.SetActive(false);
     }
 
-    private void FixedUpdate()
+    private bool _isGameFinish = false;
+
+    private void Update()
     {
-        _timeLimit = Mathf.Clamp(_timeLimit -= Time.fixedDeltaTime, 0f, _timeLimit);
-        _timeText.text = _timeLimit.ToString("F2");
+        if (!_isGameFinish)
+        {
+            _timeLimit = Mathf.Clamp(_timeLimit -= Time.fixedDeltaTime, 0f, _timeLimit);
+        }
+        if(_timeText) _timeText.text = _timeLimit.ToString("F2");
 
         // 起動したGimmickの数が一定数に達したら、Goalを出現させる。
-        if (_gimmickCheck.GimmickWakeUpCount == 2)
+        if (_gimmickCheck?.GimmickWakeUpCount == 2)
         {
-            _goal.gameObject.SetActive(true);
+            _goal?.gameObject.SetActive(true);
         }
         // Playerがゴールに到着したら、ゲームクリア
         if (_goal.GoalJudge) GameClear();
-        // 制限時間がなくなったら、ゲームオーバー
-        if (_timeLimit == 0f) GameOver();
+        // 制限時間 or PlayerのHPがなくなったら、ゲームオーバー
+        if (_timeLimit == 0f || _playerHP.CurrentHP == 0f) GameOver();
     }
 
     /// <summary>ゲームクリアした時にEventを呼ぶ</summary>
@@ -59,14 +69,18 @@ public class GameManager : MonoBehaviour
     {
         _gameClear.Invoke();
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        _isGameFinish = true;
         Debug.Log("Complete");
     }
 
     /// <summary>ゲームオーバーした時にEventを呼ぶ</summary>
     private void GameOver()
     {
-        Cursor.visible = true;
         _gameOver.Invoke();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        _isGameFinish = true;
         Debug.Log("Mission Failed");
     }
 }
