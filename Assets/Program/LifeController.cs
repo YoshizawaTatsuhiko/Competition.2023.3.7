1cb3221d86eef6481f50156a701d0562fffc0953
tatsupiko <identity2022@outlook.jp>
2023-03-01 23:05:03 +0900

    LifeControllerでのLifeの管理を「プロパティ→メソッド」に変更し、Lifeが増減に応じて、Eventを呼べるようにした。

---
+++ Assets/Program/LifeController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// 日本語対応
+public class LifeController : MonoBehaviour
+{
+    private float _currentLife = 1f;
+    public float CurrentLife => _currentLife;
+    [SerializeField]
+    private float _maxLife = 10f;
+    [SerializeField]
+    private UnityEvent _onReduceLife = new();
+    [SerializeField]
+    private UnityEvent _onAddLife = new();
+
+    private void Start()
+    {
+        _currentLife = _maxLife;
+    }
+
+    private void Update()
+    {
+        if (_currentLife == 0f) Destroy(gameObject);
+    }
+
+    /// <summary>Lifeの値を変化させる</summary>
+    /// <param name="changeValue">変化させる値</param>
+    /// <param name="conditions">true -> Life を changeValue 減らす | false -> Life を changeValue 増やす</param>
+    public void ChangeLife(float changeValue, bool conditions = true)
+    {
+        if (conditions)
+        {
+            _currentLife -= changeValue;
+            _onReduceLife.Invoke();
+        }
+        else
+        {
+            _currentLife += changeValue;
+            _onAddLife.Invoke();
+        }
+        Debug.Log($"{gameObject.name}'s Health = {_currentLife}");
+    }
+}
